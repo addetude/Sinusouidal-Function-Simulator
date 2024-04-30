fe4c4a3d11116915458807b469b25a27737b24a9
addetude <addelue@gmail.com>
2024-04-30 09:38:28 -0400

    added methods

--- sinusouidal.java
+++ sinusouidal.java
@@ -3,19 +3,60 @@ import java.awt.*;
 import javax.swing.event.*;
 import java.awt.event.*;
 
-public class sinusouidal{
-//implements ActionListener, KeyListener, MouseListener, MouseMotionListener{
+public class sinusouidal implements ActionListener, KeyListener, MouseListener, MouseMotionListener{
 	
 	//Properties
-	JFrame theframe;
-	JPanel thepanel;
+	JFrame theframe = new JFrame("Sinusouidal Function Simulator");
+	JPanel thepanel = new JPanel();;
 	
 	
 	//Methods
-	//hey toey
+	public void mouseExited(MouseEvent evt){
+		
+	}
+	public void mouseEntered(MouseEvent evt){
+		
+	}
+	public void mouseReleased(MouseEvent evt){
+		
+	}
+	public void mousePressed(MouseEvent evt){
+		
+	}
+	public void mouseClicked(MouseEvent evt){
+		
+	}
+	public void actionPerformed(ActionEvent evt){
+		
+	}
+	public void keyReleased(KeyEvent evt){
+	
+	}
+	public void keyPressed(KeyEvent evt){
+		
+	}
+	public void keyTyped(KeyEvent evt){
+		
+	}
+	public void mouseMoved(MouseEvent evt){
+		
+	}
+	public void mouseDragged(MouseEvent evt){
+		
+	}
+	
 	//Constructor
+	public sinusouidal(){
+		thepanel.setPreferredSize(new Dimension(800, 800));
+		theframe.pack();
+		thepanel.setVisible(true);
+		theframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 	
 	//Main Method
+	public static void main(String args[]){
+		new sinusouidal();
+	}
 	
 }
 	
