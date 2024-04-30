fd6d366c44ec95b9640e1e5cfd17b0aeda831a13
addetude <addelue@gmail.com>
2024-04-30 09:20:17 -0400

    Initial commit

---
+++ sinusouidal.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.event.*;
+import java.awt.event.*;
+
+public class sinusouidal{
+//implements ActionListener, KeyListener, MouseListener, MouseMotionListener{
+	
+	//Properties
+	JFrame theframe;
+	JPanel thepanel;
+	
+	//Methods
+	//hey toey
+	//Constructor
+	
+	//Main Method
+	
+}
+	
