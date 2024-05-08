da5536731885eb2f6d0072409726f3f3b3df0704
addetude <addelue@gmail.com>
2024-05-08 13:41:23 -0400

    simulation complete

--- simpanel.java
+++ simpanel.java
@@ -5,18 +5,54 @@ import java.awt.event.*;
 import java.io.*;
 import javax.imageio.*;
 import java.awt.image.*;
+import java.lang.Math.*;
 
 public class simpanel extends JPanel{
 	
 	//Properties
 	BufferedImage imgsin;
+	BufferedImage imggraph;
+	BufferedImage imgsin2;
+	
+	int intCount;
+	
+	int inta = 1;
+	int intk = 1;
+	int intc = 0;
+	int intd = 0;
+	
+	String strcb = "sin";
 	
 	//Methods
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		g.setColor(new Color(238,238,238));
 		g.fillRect(0,0,960,540);
-		g.drawImage(imgsin, 35, 30, null);
+		g.drawImage(imgsin, 35, 20, null);
+		g.drawImage(imggraph, 325, 15, null);
+		g.setColor(Color.BLACK);
+		g.drawRect(325, 15, 450, 450);
+		g.setColor(Color.BLACK);
+		g.drawImage(imgsin2, 795, 180, null);
+		g.setColor(new Color(40, 150, 20));
+		for(intCount = -188; intCount < 260; intCount++){
+			if(strcb == "sin"){
+				int inty = (int)(((inta*-10)*Math.sin((intk*0.01)*(intCount*1.0000000001)+intc*25)-intd*50/10)+250);
+				if(inty >= 15 && inty <= 465){
+					g.drawString("-", intCount+513, inty+3);
+				}
+			}else if(strcb == "cos"){
+				int inty = (int)(((inta*-10)*Math.cos((intk*0.01)*(intCount*1.0000000001)+intc*25)-intd*50/10)+250);
+				if(inty >= 15 && inty <= 465){
+					g.drawString("-", intCount+513, inty+3);
+				}
+			}else if(strcb == "tan"){
+				int inty = (int)(((inta*-10)*Math.tan((intk*0.01)*(intCount*1.0000000001)+intc*25)-intd*50/10)+250);
+				if(inty >= 15 && inty <= 465){
+					g.drawString("-", intCount+513, inty+3);
+				}
+			}
+		}
 	}
 	
 	//Constructor
@@ -24,6 +60,8 @@ public class simpanel extends JPanel{
 		super();
 		try{
 			imgsin = ImageIO.read(new File("SIN.png"));
+			imggraph = ImageIO.read(new File("2.png"));
+			imgsin2 = ImageIO.read(new File("new.png"));
 		}catch(IOException e){
 			System.out.println("cannot load SIN.png");
 		}
--- sinusouidal.java
+++ sinusouidal.java
@@ -1,6 +1,8 @@
 //Program Name: Sinusoudial Function Simulator 
-//Created by: Adeline Lue Song & Chloe Tai
+//Created by: Adeline Lue Sang & Chloe Tai
 //Version number: 0.1
+
+//CHECK TEXT FIELD INPUT EXCEPTION
 
 import javax.swing.*;
 import java.awt.*;
@@ -11,10 +13,7 @@ public class sinusouidal extends JFrame implements ActionListener, ChangeListene
 	
 	//PROPERTIES
 	//Variables
-	int inta = 1;
-	int intk = 1;
-	int intc = 0;
-	int intd = 0;
+	
 	
 	//Frame & Panels
 	JFrame theframe = new JFrame("Sinusouidal Function Simulator");
@@ -36,16 +35,36 @@ public class sinusouidal extends JFrame implements ActionListener, ChangeListene
 	JMenuItem simulatormenuitem = new JMenuItem("Simulator");
 	JMenuItem homemenuitem = new JMenuItem("Home");
 	
-	//Sliders & Text Fields & Labels
+	//Sliders & Text Fields & Labels & Combo Boxes
 	JSlider aslider = new JSlider(-10, 10, 0);
 	JSlider kslider = new JSlider(-10, 10, 0);
-	JSlider cslider = new JSlider();
-	JSlider dslider = new JSlider();
+	JSlider cslider = new JSlider(-10, 10, 0);
+	JSlider dslider = new JSlider(-10, 10, 0);
 	
 	JTextField afield = new JTextField("1");
 	JTextField kfield = new JTextField("1");
 	JTextField cfield = new JTextField("0");
 	JTextField dfield = new JTextField("0");
+	
+	JLabel avalue = new JLabel("a-value");
+	JLabel kvalue = new JLabel("k-value");
+	JLabel cvalue = new JLabel("c-value");
+	JLabel dvalue = new JLabel("d-value");
+	
+	JLabel eqlabel = new JLabel("equation");
+	JLabel ftlabel = new JLabel("function type");
+	JLabel yvalue = new JLabel("y-value");
+	
+	JLabel eqa = new JLabel("");
+	JLabel eqk = new JLabel("");
+	JLabel eqc = new JLabel("");
+	JLabel eqd = new JLabel("");
+	JLabel eqfunc = new JLabel("sin");
+	
+	String[] arr = {"sin", "cos", "tan"} ;
+	
+	JComboBox cb = new JComboBox(arr);
+	
 	
 	//METHODS
 	public void actionPerformed(ActionEvent evt){
@@ -70,32 +89,53 @@ public class sinusouidal extends JFrame implements ActionListener, ChangeListene
 		}else if(evt.getSource() == quizmenuitem){
 			
 		}else if(evt.getSource() == afield){
-			inta = Integer.parseInt(afield.getText());
-			aslider.setValue(inta);
+			smpanel.inta = Integer.parseInt(afield.getText());
+			aslider.setValue(smpanel.inta);
+			smpanel.repaint();
 		}else if(evt.getSource() == kfield){
-			intk = Integer.parseInt(kfield.getText());
-			kslider.setValue(intk);
+			smpanel.intk = Integer.parseInt(kfield.getText());
+			kslider.setValue(smpanel.intk);
+			smpanel.repaint();
 		}else if(evt.getSource() == cfield){
-			intc = Integer.parseInt(cfield.getText());
-			cslider.setValue(intc);
+			smpanel.intc = Integer.parseInt(cfield.getText());
+			cslider.setValue(smpanel.intc);
+			smpanel.repaint();
 		}else if(evt.getSource() == dfield){
-			intd = Integer.parseInt(dfield.getText());
-			dslider.setValue(intd);
+			smpanel.intd = Integer.parseInt(dfield.getText());
+			dslider.setValue(smpanel.intd);
+			smpanel.repaint();
+		}else if(evt.getSource() == cb){
+			smpanel.strcb = (String)cb.getSelectedItem();
+			eqfunc = eqmethfunc(smpanel.strcb);
+			smpanel.add(eqfunc);
+			smpanel.repaint();
 		}
 	}
 	public void stateChanged(ChangeEvent evt){
 		if(evt.getSource() == aslider){
-			inta = aslider.getValue();
-			afield.setText(inta+"");
+			smpanel.inta = aslider.getValue();
+			afield.setText(smpanel.inta+"");
+			eqa = eqmetha(smpanel.inta);
+			smpanel.add(eqa);
+			smpanel.repaint();
 		}else if(evt.getSource() == kslider){
-			inta = kslider.getValue();
-			kfield.setText(intk+"");
+			smpanel.intk = kslider.getValue();
+			kfield.setText(smpanel.intk+"");
+			eqk = eqmethk(smpanel.intk);
+			smpanel.add(eqk);
+			smpanel.repaint();
 		}else if(evt.getSource() == cslider){
-			inta = cslider.getValue();
-			cfield.setText(intc+"");
+			smpanel.intc = cslider.getValue();
+			cfield.setText(smpanel.intc+"");
+			eqc = eqmethc(smpanel.intc);
+			smpanel.add(eqc);
+			smpanel.repaint();
 		}else if(evt.getSource() == dslider){
-			inta = dslider.getValue();
-			dfield.setText(intd+"");
+			smpanel.intd = dslider.getValue();
+			dfield.setText(smpanel.intd+"");
+			eqd = eqmethd(smpanel.intd);
+			smpanel.add(eqd);
+			smpanel.repaint();
 		}
 	}
 	public void mouseClicked(MouseEvent evt){
@@ -125,6 +165,7 @@ public class sinusouidal extends JFrame implements ActionListener, ChangeListene
 		quizmenuitem.addActionListener(this);
 		simulatormenuitem.addActionListener(this);
 		homemenuitem.addActionListener(this);
+		cb.addActionListener(this);
 		
 		theframe.setJMenuBar(themenubar);
 		
@@ -139,13 +180,13 @@ public class sinusouidal extends JFrame implements ActionListener, ChangeListene
 		//Sliders & Labels & Buttons
 		aslider.setSize(250, 20);
 		aslider.setLocation(50, 100);
-		aslider.setValue(inta);
+		aslider.setValue(smpanel.inta);
 		aslider.addChangeListener(this);
 		smpanel.add(aslider);
 		
 		kslider.setSize(250, 20);
 		kslider.setLocation(50, 200);
-		kslider.setValue(intk);
+		kslider.setValue(smpanel.intk);
 		kslider.addChangeListener(this);
 		smpanel.add(kslider);
 		
@@ -183,12 +224,117 @@ public class sinusouidal extends JFrame implements ActionListener, ChangeListene
 		dfield.addActionListener(this);
 		smpanel.add(dfield);
 		
+		avalue.setSize(100, 50);
+		avalue.setLocation(155, 55);
+		smpanel.add(avalue);
 		
+		kvalue.setSize(100, 50);
+		kvalue.setLocation(155, 155);
+		smpanel.add(kvalue);
+		
+		cvalue.setSize(100, 50);
+		cvalue.setLocation(155, 255);
+		smpanel.add(cvalue);
+		
+		dvalue.setSize(100, 50);
+		dvalue.setLocation(155, 355);
+		smpanel.add(dvalue);
+		
+		eqlabel.setSize(100, 50);
+		eqlabel.setLocation(838, 130);
+		smpanel.add(eqlabel);
+		
+		ftlabel.setSize(100, 50);
+		ftlabel.setLocation(830, 260);
+		smpanel.add(ftlabel);
+		
+		cb.setSize(60, 25);
+		cb.setLocation(837, 300);
+		smpanel.add(cb);
+		
+		eqa = eqmetha(smpanel.inta);
+		smpanel.add(eqa);
+		
+		eqk = eqmethk(smpanel.intk);
+		smpanel.add(eqk);
+		
+		eqc = eqmethc(smpanel.intc);
+		smpanel.add(eqc);
+		
+		eqd = eqmethd(smpanel.intd);
+		smpanel.add(eqd);
+		
+		eqfunc = eqmethfunc(smpanel.strcb);
+		smpanel.add(eqfunc);
+
 	}
 	
 	//Main Method
 	public static void main(String args[]){
 		new sinusouidal();
 	}
+	
+	public JLabel eqmetha(int inta){
+		eqa.setText("");
+		
+		JLabel equation = new JLabel();
+		
+		equation.setSize(100, 50);
+		equation.setForeground(Color.RED);
+		equation.setText(inta+"");
+		equation.setLocation(784, 168);
+		
+		return equation;
+	}
+	public JLabel eqmethk(int intk){
+		eqk.setText("");
+		
+		JLabel equation = new JLabel();
+		
+		equation.setSize(100, 50);
+		equation.setForeground(new Color(250, 100, 210));
+		equation.setText(intk+"");
+		equation.setLocation(839, 168);
+		
+		return equation;	
+	}
+	public JLabel eqmethc(int intc){
+		eqc.setText("");
+		
+		JLabel equation = new JLabel();
+		
+		equation.setSize(100, 50);
+		equation.setForeground(Color.BLUE);
+		equation.setText(intc+"");
+		equation.setLocation(883, 168);
+		
+		return equation;
+	}
+	public JLabel eqmethd(int intd){
+		eqd.setText("");
+		
+		JLabel equation = new JLabel();
+		
+		equation.setSize(100, 50);
+		equation.setForeground(new Color(240, 120, 50));
+		equation.setText(intd+"");
+		equation.setLocation(928, 168);
+		
+		return equation;
+	}
+	public JLabel eqmethfunc(String strfunc){
+		eqfunc.setText("");
+		
+		JLabel equation = new JLabel();
+		
+		equation.setSize(100, 50);
+		equation.setForeground(Color.BLACK);
+		equation.setText(strfunc+"");
+		equation.setLocation(812, 168);
+		
+		return equation;
+		
+	}
+
 }
 	
